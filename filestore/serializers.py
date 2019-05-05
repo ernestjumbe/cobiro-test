00d244a7e60499e28b88f894ee4ada784fd1ec8f
Ernest Jumbe <e2jeyy@gmail.com>
2019-05-05 20:43:53 +0200

    List store products in api view

--- filestore/serializers.py
+++ filestore/serializers.py
@@ -3,3 +3,10 @@ from rest_framework import serializers
 class StoreSerializer(serializers.Serializer):
 
     store_name = serializers.CharField()
+
+class StoreProductsSerializer(serializers.Serializer):
+
+    title = serializers.CharField()
+    link = serializers.CharField()
+    description = serializers.CharField()
+    image_link = serializers.CharField()
--- filestore/views.py
+++ filestore/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # DRF imports
 from rest_framework import generics
 
-from .serializers import StoreSerializer
+from .serializers import StoreSerializer, StoreProductsSerializer
 
 from store_client import CobiroStoreAPI
 
@@ -15,3 +15,12 @@ class ListStores(generics.ListAPIView):
 
     queryset = store_api.get_stores()
     serializer_class = StoreSerializer
+
+class ListStoreProducts(generics.ListAPIView):
+
+    serializer_class = StoreProductsSerializer
+
+    def get_queryset(self):
+        store_name = " ".join(self.kwargs["store_slug"].split("-"))
+        queryset = store_api.get_store_products(store_name)
+        return queryset
