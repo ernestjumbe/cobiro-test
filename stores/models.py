fa45efb5cba6d0b4ddcdaa9b3bc2b3b3304313d5
Ernest Jumbe <e2jeyy@gmail.com>
2019-05-05 22:39:30 +0200

    Created store and product model

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+
+    store_name = models.CharField("store_name", max_length=254)
+
+
+class Product(models.Model):
+
+    title = models.CharField("title", max_length=254)
+    link = models.URLField("link")
+    description = models.TextField("description")
+    image_link = models.URLField("image_link")
+    
